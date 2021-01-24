2951451b4894a47f38ce2eafb77003d199a16923
moundu <munenemuondu@gmail.com>
2021-01-24 10:38:28 +0300

    I have practised button

---
+++ practise2.py
@@ -0,0 +1,8 @@
+import sys
+from PyQt5 import QtWidgets as qtw
+from PyQt5 import QtGui as qtg
+from PyQt5 import QtCore as QtCore
+
+class DialogWindow(qtw.QtWidget):
+    def __init__(self):
+        super().__init()
